98e7d3d2ee9320372ca90e7f89fa3cbce63a3f3e
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2025-01-17 19:21:09 -0600

    [Update] Implement the hook useSEO in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { Suspense, useState } from 'react'
 import './App.css'
 import Scene from './components/Scene'
 import Info from './components/Info'
+import useSEO from './hooks/useSEO';
 
 function App() {
-
+  useSEO(
+    'Volkswagen Jetta 2019',
+    'Descubre las características del Volkswagen Jetta 2019, un vehículo moderno y elegante con un diseño aerodinámico y tecnología de vanguardia.'
+);
   return (
     <>
       <div className='scene_container'>
